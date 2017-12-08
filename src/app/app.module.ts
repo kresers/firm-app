29b078dd4751e8842c5f298f4b4b53dd5f5fbad8
MWSebastienT <s.toursel@mentalworks.fr>
2017-12-08 14:44:15 +0100

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FilterComponent } from './filter/filter.component';
 import { EnterpriseComponent } from './enterprise/enterprise.component';
+import { TopNavbarComponent } from './top-navbar/top-navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FilterComponent,
-    EnterpriseComponent
+    EnterpriseComponent,
+    TopNavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
