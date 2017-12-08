bb8df707fb098a0ec73d6a006bc170cb98a77f70
kresers <valentinregnier@free.fr>
2017-12-08 14:30:23 +0100

    service test + api test

--- src/app/api-firm.service.ts
+++ src/app/api-firm.service.ts
@@ -9,4 +9,7 @@ export class ApiFirmService {
     return 'salut';
   }
 
+    getMyServ(): void {
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { FilterComponent } from './filter/filter.component';
-import { EnterpriseComponent } from './enterprise/enterprise.component';
-import { ApiFirmService } from './api-firm.service';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {FilterComponent} from './filter/filter.component';
+import {EnterpriseComponent} from './enterprise/enterprise.component';
+import {ApiFirmService} from './api-firm.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FilterComponent,
-    EnterpriseComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [ApiFirmService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FilterComponent,
+        EnterpriseComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule
+    ],
+    providers: [ApiFirmService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
