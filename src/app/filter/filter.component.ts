3ca4dbed5d69af186c2d222cad08fe6b94d6496b
MWSebastienT <s.toursel@mentalworks.fr>
2017-12-22 12:11:41 +0100

    les filters

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -9,10 +9,19 @@ import {HttpClient} from '@angular/common/http';
 })
 export class FilterComponent implements OnInit {
     zipCode = [];
+    displayZipCodeForm = false;
+    departement = [];
+    displayDepartementForm = false;
+    codeApe = [];
+    displayCodeApeForm = false;
+    categEntHard = ['PME', 'ETI', 'GE'];
+    categEnt = [];
+    displayCategEnt = false;
+    categError = false;
     results: string[];
     loaded = false;
     listTest = [];
-    displayZipCodeForm = false;
+
 
     constructor(private  http: HttpClient, private apiFirmService: ApiFirmService) {
     }
@@ -36,14 +45,86 @@ export class FilterComponent implements OnInit {
 
     deleteZipCode(idCode): void {
         console.log(idCode);
-        this.zipCode.splice(idCode,1);
+        this.zipCode.splice(idCode, 1);
     }
 
     onSelect(): void {
         if (this.displayZipCodeForm) {
             this.displayZipCodeForm = false;
-        }else {
+        } else {
             this.displayZipCodeForm = true;
+        }
+    }
+
+    addDepartement(code: string): void {
+        this.departement.push(code);
+    }
+
+    deleteDepartement(idCode): void {
+        console.log(idCode);
+        this.departement.splice(idCode, 1);
+    }
+
+    onSelectDep(): void {
+        if (this.displayDepartementForm) {
+            this.displayDepartementForm = false;
+        } else {
+            this.displayDepartementForm = true;
+        }
+    }
+
+    addCodeApe(code: string): void {
+        this.codeApe.push(code);
+    }
+
+    deleteCodeApe(idCode): void {
+        console.log(idCode);
+        this.codeApe.splice(idCode, 1);
+    }
+
+    onSelectCodeApe(): void {
+        if (this.displayCodeApeForm) {
+            this.displayCodeApeForm = false;
+        } else {
+            this.displayCodeApeForm = true;
+        }
+    }
+
+    addCategEnt(code: string): void {
+
+
+        const status = this.checkValue(code, this.categEnt)
+
+
+        if (status === false) {
+            this.categEnt.push(code);
+        } else {
+            this.categError = true;
+        }
+    }
+
+    checkValue(value, array) {
+        let status = false;
+        for (let i = 0; i < array.length; i++) {
+            const name = array[i];
+            if (name === value) {
+                status = true;
+                break;
+            }
+        }
+        return status;
+    }
+
+    deleteCategEnt(idCode): void {
+        console.log(idCode);
+        this.categEnt.splice(idCode, 1);
+    }
+
+    onSelectCategEnt(): void {
+        if (this.displayCategEnt) {
+            this.displayCategEnt = false;
+        } else {
+            this.displayCategEnt = true;
         }
     }
 }
