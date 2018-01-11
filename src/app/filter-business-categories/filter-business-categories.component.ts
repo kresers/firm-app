51593097a65b44a57f743ce0aad5c7ed97bf1abf
kresers <valentinregnier@free.fr>
2018-01-11 13:56:38 +0100

    add categories enterprise filter

---
+++ src/app/filter-business-categories/filter-business-categories.component.ts
@@ -0,0 +1,56 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {ApiFirmService} from '../api-firm.service';
+
+@Component({
+    selector: 'app-filter-business-categories',
+    templateUrl: './filter-business-categories.component.html',
+    styleUrls: ['./filter-business-categories.component.css']
+})
+export class FilterBusinessCategoriesComponent implements OnInit {
+
+    categEnt = [];
+    categEntHard = ['PME', 'ETI', 'GE'];
+    categError = false;
+    displayCategEnt = false;
+    @Output() outputListCateg = new EventEmitter<{}>(); // #SEB  the value of this output is transmit to the app.componenet.ts
+
+    constructor(private apiFirmService: ApiFirmService) {
+    }
+
+    ngOnInit() {
+    }
+
+    /* this function add categ enterprise in the list of filter categ enterprise */
+    addCategEnt(code: string): void {
+        const status = this.apiFirmService.checkValue(code, this.categEnt);
+        if (status === false) {
+            this.categEnt.push(code);
+            this.categError = false;
+            this.updateParentListCateg();
+        } else {
+            this.categError = true;
+        }
+    }
+
+    /* this function delete categ enterprise in the list of filter categ enterprise */
+    deleteCategEnt(idCode): void {
+        this.categEnt.splice(idCode, 1);
+        this.updateParentListCateg();
+    }
+
+
+    /* this function display or not the form categ enterprise */
+    onSelectCategEnt(): void {
+        if (this.displayCategEnt) {
+            this.displayCategEnt = false;
+            this.categError = false;
+        } else {
+            this.displayCategEnt = true;
+        }
+    }
+
+    /* #SEB  this functon update the value in the app.component.ts */
+    updateParentListCateg() {
+        this.outputListCateg.emit(this.categEnt);
+    }
+}
