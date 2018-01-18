d9e9da2bd30d4b6a6c27f78b3efefefe2bdf5e6b
MWSebastienT <s.toursel@mentalworks.fr>
2018-01-18 10:43:52 +0100

    fix

--- src/app/export/export.component.ts
+++ src/app/export/export.component.ts
@@ -15,30 +15,34 @@ export class ExportComponent implements OnInit {
 
     constructor(private apiFirmService: ApiFirmService) {
     }
-  
-    downloadCsv () {
-      const options = {
-          fieldSeparator: ';',
-          quoteStrings: '"',
-          decimalseparator: '.',
-          showLabels: true,
-          showTitle: true,
-          useBom: true
-      };
-      this.apiFirmService.getEnterpriseByParameters().subscribe(data => {
-          data['records'].forEach((value) => {
-              const enterprise = new Enterprise(value.fields.siren,
-                                                  value.fields.nic,
-                                                  value.fields.l1_normalisee,
-                                                  value.fields.l2_normalisee,
-                                                  value.fields.l3_normalisee,
-                                                  value.fields.l4_normalisee, );
-              this.listEnterprises.push(enterprise);
-          });
-          return new Angular2Csv(this.listEnterprises, 'fileName' , options);
-      });
+
+    ngOnInit() {
+
     }
-  
+
+    downloadCsv() {
+        const options = {
+            fieldSeparator: ';',
+            quoteStrings: '"',
+            decimalseparator: '.',
+            showLabels: true,
+            showTitle: true,
+            useBom: true
+        };
+        this.apiFirmService.getEnterpriseByParameters().subscribe(data => {
+            data['records'].forEach((value) => {
+                const enterprise = new Enterprise(value.fields.siren,
+                    value.fields.nic,
+                    value.fields.l1_normalisee,
+                    value.fields.l2_normalisee,
+                    value.fields.l3_normalisee,
+                    value.fields.l4_normalisee,);
+                this.listEnterprises.push(enterprise);
+            });
+            return new Angular2Csv(this.listEnterprises, 'fileName', options);
+        });
+    }
+
     downloadJson() {
         this.apiFirmService.getEnterpriseByParameters().subscribe(data => {
             data['records'].forEach((value) => {
